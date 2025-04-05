173625c5961bc51bf62641aa6e22ac08164c83bc
Devankshi Deshmukh <68735282+curry-dev@users.noreply.github.com>
2025-04-05 12:54:21 -0700

    Created basic safemap layout in html

--- src/app/components/safemap/safemap.component.ts
+++ src/app/components/safemap/safemap.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-safemap',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './safemap.component.html',
   styleUrl: './safemap.component.css'
 })
 export class SafemapComponent {
+  wherefrom = new FormControl('');
+  whereto = new FormControl('');
 
+  constructor(private _apiservice: ApiService) {}
+
+  getRoute() {
+
+  }
 }
