86ef25f068e7adad498843642ae440f070a75ddd
Devankshi Deshmukh <68735282+curry-dev@users.noreply.github.com>
2025-04-05 12:28:46 -0700

    Created new component profile and made navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SafemapComponent } from './components/safemap/safemap.component';
 import { PlantripComponent } from './components/plantrip/plantrip.component';
 import { FeelingluckyComponent } from './components/feelinglucky/feelinglucky.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
     {
@@ -25,6 +26,10 @@ export const routes: Routes = [
     {
         path: 'feelinglucky', 
         component: FeelingluckyComponent
+    },
+    {
+        path: 'profile', 
+        component: ProfileComponent
     }
 ];
 
