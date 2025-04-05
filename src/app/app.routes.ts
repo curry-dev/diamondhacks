620fd7fe834f5f1e9658de9cfe9b0871b876347e
Devankshi Deshmukh <68735282+curry-dev@users.noreply.github.com>
2025-04-05 11:58:46 -0700

    Added routes and bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SafemapComponent } from './components/safemap/safemap.component';
+import { PlantripComponent } from './components/plantrip/plantrip.component';
+import { FeelingluckyComponent } from './components/feelinglucky/feelinglucky.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home', 
+        component: HomeComponent
+    },
+    {
+        path: 'safemap', 
+        component: SafemapComponent
+    },
+    {
+        path: 'plantrip', 
+        component: PlantripComponent
+    },
+    {
+        path: 'feelinglucky', 
+        component: FeelingluckyComponent
+    }
+];
+
+export default routes;
