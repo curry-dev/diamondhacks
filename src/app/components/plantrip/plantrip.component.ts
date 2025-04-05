8748eedb9e318b6130ffe66d237570e69287ed75
Devankshi Deshmukh <68735282+curry-dev@users.noreply.github.com>
2025-04-05 13:05:48 -0700

    Created basic layout for plantrip in html

--- src/app/components/plantrip/plantrip.component.ts
+++ src/app/components/plantrip/plantrip.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-plantrip',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './plantrip.component.html',
   styleUrl: './plantrip.component.css'
 })
-export class PlantripComponent {
 
+export class PlantripComponent {
+  whereto = new FormControl('');
+  budget = new FormControl('');
+  numpeople = new FormControl('');
+
+  constructor(private _apiservice: ApiService) {}
+
+  getTrip() {
+
+  }
 }
